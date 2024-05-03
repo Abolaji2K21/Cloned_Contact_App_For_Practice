fcec33a33a85cd3cef736482939ee845add6ff02
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-05-03 17:13:21 +0100

    penisAlmost done

--- src/main/java/africa/semicolon/controller/ApprovalController.java
+++ src/main/java/africa/semicolon/controller/ApprovalController.java
@@ -5,6 +5,8 @@ import africa.semicolon.data.models.Approval;
 import africa.semicolon.data.models.Status;
 import africa.semicolon.dtos.requests.ShareContactDto;
 import africa.semicolon.dtos.response.ApiResponse;
+import africa.semicolon.dtos.response.ChangeStatusResponseDTO;
+import africa.semicolon.dtos.response.ShareContactDtoResponse;
 import africa.semicolon.services.ApprovalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
@@ -25,26 +28,26 @@ public class ApprovalController {
     @PostMapping("/share")
     public ResponseEntity<?> shareContact(@RequestBody ShareContactDto shareContactDto) {
         try {
-            approvalService.share(shareContactDto);
+            ShareContactDtoResponse result=approvalService.share(shareContactDto);
                 return new ResponseEntity<>(new ApiResponse(true,  result),OK);
-        } catch (BigContactException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (BigContactException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()),BAD_REQUEST);
         }
     }
 
     @GetMapping("/getApprovals/{userId}")
-    public ResponseEntity<?> getApprovals(@PathVariable String userId) {
-        List<Approval> approvals = approvalService.getApprovals(userId);
-        return ResponseEntity.ok(approvals);
+    public ResponseEntity<?> getApprovals(@PathVariable(name = "userId") String userId) {
+        List<Approval> result = approvalService.getApprovals(userId);
+        return new ResponseEntity<>(new ApiResponse(true,  result),OK);
     }
 
     @PostMapping("/changeStatus/{approvalId}/{userId}")
     public ResponseEntity<?> changeStatus(@PathVariable String approvalId, @PathVariable String userId, @RequestParam Status status) {
         try {
-            approvalService.changeStatus(status, approvalId, userId);
-            return ResponseEntity.ok("Status changed successfully");
-        } catch (BigContactException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            ChangeStatusResponseDTO result  =approvalService.changeStatus(status, approvalId, userId);
+            return new ResponseEntity<>(new ApiResponse(true,  result),OK);
+        } catch (BigContactException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()),BAD_REQUEST);
         }
     }
 }
